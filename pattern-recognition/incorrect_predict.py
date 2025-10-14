fce9222a5b9e1acb61fe00b076f7b931530cf3d5
Pakin49 <pakin.pan@proton.me>
2025-10-15 04:11:07 +0700

    pattern python env

--- pattern-recognition/incorrect_predict.py
+++
@@ -1,8 +0,0 @@
-## this is the correct prediction count
-count = 0;
-for predict in predicts:
-  if predict[0] > 0.5 and predict[2] == 'W':
-    count = count + 1
-  elif predict[1] > 0.5 and predict[2] == 'M':
-    count = count + 1
-print(len(predicts)-count)
